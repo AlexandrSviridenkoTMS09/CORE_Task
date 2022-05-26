cf35efc630701f2521b4de3d1280ea91537c9c67
AlexandrSviridenkoTMS09 <91564222+AlexandrSviridenkoTMS09@users.noreply.github.com>
2022-05-26 11:27:13 +0300

    Таск4 исправлен согласно замечаний

--- src/main/java/Task4/EnumDays.java
+++ src/main/java/Task4/EnumDays.java
@@ -7,7 +7,6 @@ import static Task4.Weekday.print;
 public class EnumDays {
     public static void main(String[] args) {
         print(introductoryInformation());
-
     }
 
     public static int introductoryInformation() {
@@ -19,49 +18,6 @@ public class EnumDays {
     }
 }
 
-enum Weekday {
-    MONDAY(false, "MON"),
-    TUESDAY(false, "TUE"),
-    WEDNESDAY(false, "WED"),
-    THURSDAY(false, "THU"),
-    FRIDAY(false, "FRI"),
-    SATURDAY(true, "SAT"),
-    SUNDAY(true, "SUN");
 
-    private final boolean weekend;
-    private final String abbreviation;
-
-    Weekday(boolean weekend, String abbreviation) {
-        this.weekend = weekend;
-        this.abbreviation = abbreviation;
-    }
-
-
-    public String getAbbreviation() {
-        return this.abbreviation;
-    }
-
-    public boolean isWeekend() {
-        return this.weekend;
-    }
-
-    public static void print(int num) {
-        Weekday day;
-
-        if (num > 7 || num < 1) {
-            System.out.println("Wrong choice");
-            return;
-        }
-
-        day = Weekday.values()[num - 1];
-        System.out.println(day.getInfo());
-    }
-
-    public String getInfo() {
-        return name() +
-                " (" + abbreviation + ") " +
-                (weekend ? "is a weekend" : "is not a weekend");
-    }
-}
 
 
